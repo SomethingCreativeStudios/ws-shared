fffd56e7522a7b9f07a86c1898a6b118742b0d65
Eric <eric-417@live.com>
2021-10-30 13:24:01 -0400

    adding shared

---
+++ src/types/WSCard.ts
@@ -0,0 +1,45 @@
+export interface WSCard {
+  name: {
+    en: string;
+    jp: string;
+  };
+
+  color: string;
+  rarity: string;
+  cardNo: string;
+  set: string;
+  side: string;
+  type: string;
+  level: number;
+  cost: number;
+  power: number;
+  soul: number;
+  trait: {
+    en: string[];
+    jp: string[];
+  };
+  triggers: string;
+  flavor: string;
+  text: string;
+  vartext: {
+    Constant: string;
+    Action: string;
+  };
+  typetext: TypeText[];
+  superDetailedText: DetailedText[];
+}
+
+export interface TypeText {
+  type: string;
+  effect: string;
+  cost?: {
+    stock: string;
+    other: string;
+  };
+}
+
+export interface DetailedText {
+  type: string;
+  effect: string;
+  cost: { action: string; value: number }[];
+}
